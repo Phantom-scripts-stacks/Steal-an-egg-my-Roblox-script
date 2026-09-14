41713934c02260f682f67b7a149f8f2f3b8e7c0b
larayosneohan-sys <larayosneohan@gmail.com>
2026-09-15 00:09:46 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -8,13 +8,20 @@ print("Loaded: MAMAAAA")
 -- [ SCRIPT 1 ]
 task.spawn(function()
     pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/miirandahub/loader/main/stealaeggs"))()
     end)
 end)
 
 -- [ SCRIPT 2 ]
 task.spawn(function()
     pcall(function()
-        -- loadstring(game:HttpGet("https://raw.githubusercontent.com/miirandahub/loader/main/stealaeggs"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/lennonxscripts/lennonhubv2/refs/heads/main/stealaneggv2"))()
+    end)
+end)
+
+-- [ SCRIPT 3 ]
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/65bf3459d87ba3ac46350e154b640929.lua"))()
     end)
 end)
