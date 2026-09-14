5603bbecf5b16baa2214aa3bfef2e66cbc2355c5
larayosneohan-sys <larayosneohan@gmail.com>
2026-09-15 00:33:53 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,40 +1,122 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/main/dist/main.lua"))()
 
-local Window = Rayfield:CreateWindow({
-   Name = "MAMAAAA Hub",
-   LoadingTitle = "Loading Master Hub...",
-   LoadingSubtitle = "By UNKNOWN",
-   ConfigurationSaving = { Enabled = false }
+local Window = WindUI:CreateWindow({
+    Title = "its_me-bj Hub",
+    Icon = "shield",
+    Author = "UNKNOWN",
+    Folder = "MAMAAAAHub",
+    Size = UDim2.fromOffset(500, 350),
+    Theme = "Dark",
+    Acrylic = false
 })
 
-local MainTab = Window:CreateTab("Main Scripts", 4483362458)
-
--- [ BUTTON 1 ]
-MainTab:CreateButton({
-   Name = "Steal an Egg Script 1",
-   Callback = function()
-       pcall(function()
-           loadstring(game:HttpGet("https://raw.githubusercontent.com/eiltrunduhub/loader/main/stealeggs"))()
-       end)
-   end,
+local MainTab = Window:Tab({
+    Title = "Main Scripts",
+    Icon = "home"
 })
 
--- [ BUTTON 2 ]
-MainTab:CreateButton({
-   Name = "Steal an Egg Script 2",
-   Callback = function()
-       pcall(function()
-           loadstring(game:HttpGet("https://raw.githubusercontent.com/miirandahub/loader/main/stealaeggs"))()
-       end)
-   end,
+-- MIRANDA
+MainTab:Button({
+    Title = "Steal an Egg Script 1",
+    Callback = function()
+        pcall(function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/miirandahub/loader/main/stealaeggs"))()
+        end)
+    end
 })
 
--- [ BUTTON 3 ]
-MainTab:CreateButton({
-   Name = "Script 3",
-   Callback = function()
-       pcall(function()
-           loadstring(game:HttpGet("https://raw.githubusercontent.com/lennonxscripts/lennonhubv2/refs/heads/main/stealaneggv2"))()
-       end)
-   end,
+-- LENNON
+MainTab:Button({
+    Title = "Steal an Egg Script 2",
+    Callback = function()
+        pcall(function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/lennonxscripts/lennonhubv2/refs/heads/main/stealaneggv2"))()
+        end)
+    end
+})
+
+-- RENE BATERBONIA
+MainTab:Button({
+    Title = "Script 3",
+    Callback = function()
+        pcall(function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/sabscrip-arch/srver/refs/heads/main/Stealanegg"))()
+        end)
+    end
+})
+
+-- [ BUTTON 4 ]
+MainTab:Button({
+    Title = "Script Slot 4",
+    Callback = function()
+        pcall(function()
+            -- I-paste ang link diri puhon
+        end)
+    end
+})
+
+-- [ BUTTON 5 ]
+MainTab:Button({
+    Title = "Script Slot 5",
+    Callback = function()
+        pcall(function()
+            -- I-paste ang link diri puhon
+        end)
+    end
+})
+
+-- [ BUTTON 6 ]
+MainTab:Button({
+    Title = "Script Slot 6",
+    Callback = function()
+        pcall(function()
+            -- I-paste ang link diri puhon
+        end)
+    end
+})
+
+-- [ BUTTON 7 ]
+MainTab:Button({
+    Title = "Script Slot 7",
+    Callback = function()
+        pcall(function()
+            -- I-paste ang link diri puhon
+        end)
+    end
+})
+
+-- [ BUTTON 8 ]
+MainTab:Button({
+    Title = "Script Slot 8",
+    Callback = function()
+        pcall(function()
+            -- I-paste ang link diri puhon
+        end)
+    end
+})
+
+-- [ BUTTON 9 ]
+MainTab:Button({
+    Title = "Script Slot 9",
+    Callback = function()
+        pcall(function()
+            -- I-paste ang link diri puhon
+        end)
+    end
+})
+
+-- [ BUTTON 10 ]
+MainTab:Button({
+    Title = "Script Slot 10",
+    Callback = function()
+        pcall(function()
+            -- I-paste ang link diri puhon
+        end)
+    end
+})
+
+WindUI:Notify({
+    Title = "MAMAAAA Hub",
+    Content = "Na-load successfully ang 10 Slots!",
+    Duration = 3
 })
