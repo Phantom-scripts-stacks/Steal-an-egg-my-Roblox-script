19b6497916d8a58da2fdf27475d03d41ea86281d
larayosneohan-sys <larayosneohan@gmail.com>
2026-09-14 23:46:09 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,27 @@
+-- ==========================================
+-- SCRIPT TITLE : MAMAAAA
+-- CREATED BY   : UNKNOWN
+-- ==========================================
+
+print("Loaded: put your script title here")
+
+-- [ SCRIPT 1 ]
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("put your script link here"))()
+    end)
+end)
+
+-- [ SCRIPT 2 ]
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("put your script link here"))()
+    end)
+end)
+
+-- [ SCRIPT 3 ]
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("put your script link here"))()
+    end)
+end)
